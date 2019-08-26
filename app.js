885cb7bb14e60445c5a2c611bc22d2b987ee6918
gmalicky <34336799+gmalicky@users.noreply.github.com>
2019-08-27 00:07:20 +0200

    Update travis config

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const favicon = require('serve-favicon');
 const db = require('./db/dbApi');
 
